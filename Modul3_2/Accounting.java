d965a311af7e40f4904dd8555232167459e616c4
Sofiia Bondarenko <sofiiabondarenko91@gmail.com>
2019-07-23 18:06:13 +0300

    v2 added  code to task 2
    v2

---
+++ Modul3_2/Accounting.java
@@ -0,0 +1,13 @@
+package Modul_3.Modul3_2;/*task 2 Write a class named Accounting which contains a method named pay.
+The method should accept a number of working hours and a per hour payment.
+It should return a calculated payment. For example pay(1.5, 10)
+should return 15.Write another class with the main method which will test the Accounting class by calling the pay method */
+import java.util.Scanner;
+
+public class Accounting {
+
+    public int pay(int workHours, int paidHours) {
+        int sum = workHours * paidHours;
+        return sum;
+    }
+}
---
+++ Modul3_2/Calc.java
@@ -0,0 +1,24 @@
+package Modul_3.Modul3_2;//module 2 task 2
+
+import java.util.Scanner;
+
+public class Calc {
+    public static void main(String [] args){
+
+        Accounting rate = new Accounting();
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Enter work hours: ");
+        int workHours = in.nextInt();
+
+        System.out.println("Enter paid hours: ");
+        int paidHours = in.nextInt();
+
+        System.out.println(rate.pay(workHours, paidHours));
+
+
+
+
+
+    }
+}
