3f3b2d033af4476505bbbe30d97a95dc24d3f88a
Sofiia Bondarenko <sofiiabondarenko91@gmail.com>
2019-07-24 17:55:14 +0300

    v3 added try catch
    try catch

--- Modul3_2/Calc.java
+++ Modul3_2/Calc.java
@@ -1,5 +1,6 @@
 package Modul_3.Modul3_2;//module 2 task 2
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Calc {
@@ -8,13 +9,18 @@ public class Calc {
         Accounting rate = new Accounting();
         Scanner in = new Scanner(System.in);
 
-        System.out.println("Enter work hours: ");
-        int workHours = in.nextInt();
+        try{
+            System.out.println("Enter work hours: ");
+            int workHours = in.nextInt();
 
-        System.out.println("Enter paid hours: ");
-        int paidHours = in.nextInt();
+            System.out.println("Enter paid hours: ");
+            int paidHours = in.nextInt();
 
-        System.out.println(rate.pay(workHours, paidHours));
+            System.out.println(rate.pay(workHours, paidHours));
+        } catch (InputMismatchException a){
+            System.out.println("Error. Enter number");
+        }
+
 
 
 
