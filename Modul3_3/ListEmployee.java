6a44ac6cd8c27338194c46d0bd04a35576f651e9
Sofiia Bondarenko <sofiiabondarenko91@gmail.com>
2019-07-25 17:27:31 +0300

    added metods and constructors

--- Modul3_3/ListEmployee.java
+++ Modul3_3/ListEmployee.java
@@ -1,10 +1,23 @@
-//package Modul3_3;
-//
-//public class ListEmployee {
-//
-//    Employee info = new Employee();
-//
-//
-//
-//    }
-//}
+package Modul3_3;
+
+
+import java.util.Random;
+
+public class ListEmployee {
+
+    public static void main(String[] args) {
+
+    Employee person1 = new Employee("Katya ", "Lavanova ", 72245);
+
+    person1.name = "Ivan ";
+    person1.surname = "Petrov ";
+
+    //Employee person2 = new Employee();
+
+    System.out.println ("There is an employee: " + person1.print() + person1.getId());
+
+    //System.out.println ("There is an employee: " + person2.setInfo("Petya ", "Ivanov ", 52245) );
+    }
+
+
+}
