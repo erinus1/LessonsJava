720684f4ebf8cf912c9202c8d0dfa75301cde79b
Sofiia Bondarenko <sofiia_bondarenko@epam.com>
2019-07-28 18:41:07 +0300

    done all points at skype

---
+++ src/Modul3_3/Employee.java
@@ -0,0 +1,43 @@
+package Modul3_3;
+import java.util.Random;
+
+
+public class Employee {
+
+    String in;
+
+    Random random = new Random();
+    int id;
+    String info;
+
+    String setInfo(String name, String surname, String status){ //method info
+
+        info = name + " " + surname + " - " + status;
+
+        return info;
+    }
+
+    String getInfo(){
+        return info;
+
+    }
+
+    Employee(){ //constructor
+        id = random.nextInt(100);
+    }
+    int setId(){
+        id = id;
+        return id;
+    }
+
+    int getId() {
+        id +=1;
+        return id;
+    }
+
+    }
+
+
+
+
+
---
+++ src/Modul3_3/ListEmployee.java
@@ -0,0 +1,26 @@
+package Modul3_3;
+import java.util.Random;
+import java.util.Scanner;
+
+public class ListEmployee {
+
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Enter your name:  ");
+        String name = in.nextLine();
+
+        System.out.println("Enter your surname:  ");
+        String surname = in.nextLine();
+
+        System.out.println("Enter your status:  ");
+        String status = in.nextLine();
+
+        Employee person1 = new Employee();
+
+        System.out.println("There is an employee: " + person1.setInfo(name, surname, status) + ", id is " + person1.getId());
+        System.out.println("Next id is " + person1.getId());
+
+    }
+}
