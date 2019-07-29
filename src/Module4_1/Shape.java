fdc6fed8ba6b8565d27f749999e7304bda36c118
erinus1 <sofiiabondarenko91@gmail.com>
2019-07-29 13:55:20 +0300

    v1

---
+++ src/Module4_1/Shape.java
@@ -0,0 +1,14 @@
+package Module4_1;
+
+public abstract class Shape {
+
+    public abstract void rectangleArea();
+
+    public abstract void squeareArea();
+
+    public abstract void circleArea();
+
+
+
+
+}
