934cef8dfc813673b4c986315d05aa88549eec55
erinus1 <sofiiabondarenko91@gmail.com>
2019-08-11 18:11:40 +0300

    v1
    v1

--- src/Module4_2/AccountActions.java
+++ src/Module4_2/AccountActions.java
@@ -2,9 +2,14 @@ package Module4_2;
 
 abstract class AccountActions {
 
-    public abstract int addMoney();
+    private int currentAmount;
+    private int newAmount;
+    private int fee;
+    private int balance;
 
-    public abstract int withDrow();
+    public abstract int addMoney(int currentSumm, int newAmount);
 
-    public abstract int calculateFee();
+    public abstract int withDraw(int newSumm);
+
+    public abstract int calculateFee(int fee);
 }
--- src/Module4_2/DebetAcc.java
+++ src/Module4_2/DebetAcc.java
@@ -1,4 +1,6 @@
 package Module4_2;
 
 public class DebetAcc {
+
+
 }
