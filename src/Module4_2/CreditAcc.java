5db0c4809ec1561248a0e8f27157f677907b7cc6
Sofiia <sofiiabondarenko91@gmail.com>
2019-08-11 21:51:11 +0300

    credit class
    hw

--- src/Module4_2/CreditAcc.java
+++ src/Module4_2/CreditAcc.java
@@ -1,4 +1,30 @@
 package Module4_2;
 
-public class CreditAcc {
+public class CreditAcc extends AccountActions{
+    public CreditAcc() {
+        currentAmount = 0;
+    }
+
+    public void printBalance() {
+        System.out.println("Your balance is " + currentAmount);
+    }
+
+    @Override
+    public float addMoney(float newAmount) {
+        currentAmount += newAmount;
+        return currentAmount;
+    }
+
+    @Override
+    public float withDraw(float newAmount) {
+        currentAmount -= (newAmount + calculateFee(1, newAmount));
+        return newAmount;
+    }
+
+    @Override
+    public float calculateFee(float fee, float amount) {
+        return fee * amount / 100;
+    }
+
+    
 }
