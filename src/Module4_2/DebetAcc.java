983d3563f4616800688506c3e53a64ed7e9f4f16
erinus1 <sofiiabondarenko91@gmail.com>
2019-08-21 18:23:56 +0300

    notify

--- src/Module4_2/DebetAcc.java
+++ src/Module4_2/DebetAcc.java
@@ -4,8 +4,6 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
-//import java.util.Observer;
-
 
 public class DebetAcc extends BankAcc implements Publisher {
     float fee = 1;
@@ -14,6 +12,12 @@ public class DebetAcc extends BankAcc implements Publisher {
     public DebetAcc() {
         subscribers = new HashMap();
     }
+
+    Observer obs = new Observer() {
+        @Override
+        public void update() {
+        }
+    };
 
     @Override
     public void setBalance(float currentAmount) {
@@ -53,9 +57,9 @@ public class DebetAcc extends BankAcc implements Publisher {
 
     @Override
     public void subscribe(Subscriber subscriber, String eventName) {
-        if (subscribers.containsKey(eventName)) {
-            ArrayList previousSubscribers = subscribers.get(eventName);
-            previousSubscribers.add(subscriber);
+        if (subscribers.containsKey(eventName)) { //поиск по ключу ивентНейм
+            ArrayList previousSubscribers = subscribers.get(eventName); //получить подписчика по ивентНемйу и добавить в список
+            previousSubscribers.add(subscriber); //
         } else {
             ArrayList<Subscriber> newSubscribers = new ArrayList();
             newSubscribers.add(subscriber);
@@ -68,6 +72,16 @@ public class DebetAcc extends BankAcc implements Publisher {
         // 1 - проверить, есть ли в подписчиках (хэш-мап) нужный ивент
         // 2 - взять всех подписчеков по имени ивента из хэш-мапа
         // 3 - пройтись по каждому из подписчиков и вызвать метод "update"
+
+        if(subscribers.containsKey(eventName)){
+            for (int i = 0; i < subscribers.size(); i++){
+                obs.update();
+
+            }
+        }
+
+
+        }
     }
 }
 
