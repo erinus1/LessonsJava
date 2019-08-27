90d39b30b60d0b8f321d682061a2462217a63939
erinus1 <sofiiabondarenko91@gmail.com>
2019-08-27 17:57:51 +0300

    PR

--- src/Module4_2/Main.java
+++ src/Module4_2/Main.java
@@ -6,22 +6,16 @@ public class Main {
         CreditAcc credit = new CreditAcc();
         DebetAcc debet = new DebetAcc();
         Human human1 = new Human();
-        
-        debet.subscribe(human1, "New event");
-        debet.notify("Set balance", "New balance");
+
+        debet.subscribe(human1, "Withdraw");
+        credit.subscribe(human1, "Add money");
 
         credit.setBalance(0);
-        credit.printBalance();
         credit.addMoney(200);
-        credit.printBalance();
         credit.withDraw(400);
-        credit.printBalance();
 
         debet.setBalance(20);
-        debet.printBalance();
         debet.addMoney(30);
-        debet.printBalance();
         debet.withDraw(10);
-        debet.printBalance();
     }
 }
