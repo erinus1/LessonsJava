5e35d14e5af8e6fad966f5d8bd6029d7fc2c9b9e
erinus1 <sofiiabondarenko91@gmail.com>
2019-09-20 17:42:14 +0300

    1

---
+++ src/Module6_2/Decode.java
@@ -0,0 +1,11 @@
+package Module6_2;
+
+public class Decode {
+
+    public String decode(String string) {
+        if (string == null || string.isEmpty()) return "";
+
+        StringBuilder builder = new StringBuilder();
+
+
+    }}
