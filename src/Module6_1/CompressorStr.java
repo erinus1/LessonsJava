1d3a4d5a0492491aa312e216f887232d60559184
Sofiia <sofiiabondarenko91@gmail.com>
2019-09-29 16:34:47 +0300

    finished

--- src/Module6_1/CompressorStr.java
+++ src/Module6_1/CompressorStr.java
@@ -7,22 +7,24 @@ public class CompressorStr {
 
     public String compress(String string) {
         StringBuilder stringBuilder = new StringBuilder();
-        outerloop:
+
         for (int i = 0; i < string.length(); i++) {
-            int count = 1;
-            if (string.charAt(i) == searchChar) { //do not count slash -WRONG
-                continue; }
-            while (i < (string.length() - 1) && string.charAt(i) == string.charAt(i + 1)) {
-                count++;
-                i++;
-
-            }
-                stringBuilder.append(count).append(string.charAt(i));
-
             if (Character.isDigit(string.charAt(i)) || string.charAt(i) == searchChar) {
-                stringBuilder.insert(stringBuilder.length() - 1, '\\');
+                stringBuilder.insert(stringBuilder.length(), '\\');
+            } else {
+                int count = 1;
+
+                while (i < (string.length() - 1) && string.charAt(i) == string.charAt(i + 1)) {
+                    count++;
+                    i++;
+
                 }
+                stringBuilder.append(count);
             }
+
+            stringBuilder.append(string.charAt(i));
+        }
+
         return stringBuilder.toString();
     }
 }
--- src/Module6_1/Main.java
+++ src/Module6_1/Main.java
@@ -7,12 +7,10 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         CompressorStr str = new CompressorStr();
 
-        System.out.println("Input: ");
-        //String resultSt = sc.nextLine();
-        //System.out.println("Compressed to " + str.compress(resultSt));
-        System.out.println("Compressed to " + str.compress("E\\5"));
+        System.out.println("To Input: ");
+        String resultSt = sc.nextLine();
+        System.out.println("Compressed to " + str.compress(resultSt));
 
-        //input: 52llk\\ >> Exception in thread "main" java.lang.NumberFormatException: For input string: "\"
     }
   }
 
