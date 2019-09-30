b4c2fac94aaad6d6a2809deef06c2a9c1b937a5e
Sofiia <sofiiabondarenko91@gmail.com>
2019-09-30 21:52:23 +0300

    home

--- src/Module6_2/Decode.java
+++ src/Module6_2/Decode.java
@@ -2,10 +2,38 @@ package Module6_2;
 
 public class Decode {
 
-    public String decode(String string) {
-        if (string == null || string.isEmpty()) return "";
+    char searchChar = '\\';
 
-        StringBuilder builder = new StringBuilder();
+    public String decompress(String string) {
+        StringBuilder stringBuilder = new StringBuilder();
 
+        // "3" => "3"
+        // "3h" => "hhh"
+        // "3h\22b" => "hhh\2bb"
 
-    }}
+        for (int i = 0; i < string.length(); i++) {
+            if (Character.isDigit(string.charAt(i))) {
+                if ((i + 1) >= string.length()) {
+                    stringBuilder.append(string.charAt(i));
+                } else if (!Character.isDigit(string.charAt(i + 1))) {
+                    int count = Character.getNumericValue(string.charAt(i));
+                    char symb = string.charAt(i + 1);
+
+                    while (count > 0) {
+                        stringBuilder.append(symb);
+                        count--;
+                    }
+                    i++;
+                } else {
+                    stringBuilder.append(string.charAt(i));
+                }
+            } else {
+//            } else if (string.charAt(i) == searchChar) {
+                stringBuilder.append(string.charAt(i));
+            }
+        }
+
+        return stringBuilder.toString();
+    }
+}
+
--- src/Module6_2/Main.java
+++ src/Module6_2/Main.java
@@ -1,4 +1,21 @@
 package Module6_2;
 
 public class Main {
+    public static void main(String[] args) {
+
+//        Scanner sc = new Scanner(System.in);
+        Decode str = new Decode();
+
+        // "3" => "3"
+        //String res1 = str.decompress("3");
+        // "3h" => "hhh"
+        //String res2 = str.decompress("3h");
+        // "3h\22b" => "hhh\2bb"
+        String res3 = str.decompress("3h\\22b");
+
+        //System.out.println("Test 1 = " + (res1.equals("3") ? true : res1));
+        //System.out.println("Test 2 = " + (res2.equals("hhh") ? true : res2));
+        System.out.println("Test 3 = " + (res3.equals("hhh\\2bb") ? true : res3));
+
+    }
 }
