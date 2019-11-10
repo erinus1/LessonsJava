e52134a09453b8f9f20aeb322e12fa660dd3ce27
Sofiia <sofiiabondarenko91@gmail.com>
2019-11-10 18:15:39 +0200

    unit test

--- src/MavenTask2/Equation.java
+++ src/MavenTask2/Equation.java
@@ -1,23 +1,28 @@
 package MavenTask2;
 
+import java.util.ArrayList;
+
 public class Equation {
 
     Equation() {
     }
 
-    public double calculate(double a, double b, double c) {
+    public ArrayList<Double> calculate(double a, double b, double c) {
 
         double d = b * b - 4 * a * c;
 
         double x1 = (-b - Math.sqrt(1.0 * d)) / (2 * a);
         double x2 = (-b + Math.sqrt(1.0 * d)) / (2 * a);
 
-            if (d == 0) {
-                System.out.println("One root" + x1); //x1
-            } else if (d > 0) {
-                System.out.println("Two roots" + x1 + ", " + x2); //x1 + x2
-            }
-            return d;
+        ArrayList<Double> result = new ArrayList<>();
+
+        result.add(x1);
+
+        if (d > 0) {
+            result.add(x2);
         }
+
+        return result;
     }
+}
 
--- src/MavenTask2/Main.java
+++ src/MavenTask2/Main.java
@@ -1,6 +1,9 @@
 package MavenTask2;
 
 import org.junit.Test;
+
+import java.util.ArrayList;
+
 import static org.junit.Assert.*;
 import static org.junit.Assert.assertEquals;
 
@@ -9,8 +12,16 @@ public class Main {
     @Test
     public static void main(String[] args) {
         Equation equation = new Equation();
-        equation.calculate(4,0,-2); //res = -0.7071067811865476 and 0.7071067811865476
+         
+        ArrayList<Double> test1 = equation.calculate(4,0,-2);
 
+        assertEquals(test1.size(), 2);
+        assertEquals(test1.get(0).toString(), String.valueOf(-0.7071067811865476));
+        assertEquals(test1.get(1).toString(), String.valueOf(0.7071067811865476));
 
+        ArrayList<Double> test2 = equation.calculate(1,2,1);
+
+        assertEquals(test2.size(), 1);
+        assertEquals(test2.get(0).toString(), String.valueOf(-1.0));
     }
 }
