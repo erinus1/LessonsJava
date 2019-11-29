c0eb835352f9b79829e341269f508d8144c80078
erinus1 <sofiiabondarenko91@gmail.com>
2019-11-29 17:29:55 +0200

    logger try1

--- src/JUnit_5_2/LinkedListTest.java
+++ src/JUnit_5_2/LinkedListTest.java
@@ -1,13 +1,14 @@
 package JUnit_5_2;
 
-import org.junit.Before;
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
 import org.junit.Test;
-
 import java.util.LinkedList;
-
 import static org.junit.Assert.*;
 
 public class LinkedListTest {
+
+    final static Logger logger = LogManager.getLogger(LinkedListTest.class);
 
     LinkedList<String> name = new LinkedList<>();
     LinkedList<Integer> years = new LinkedList<>();
@@ -23,18 +24,26 @@ public class LinkedListTest {
         name.add("Olena");
         name.add("Nickolay");
 
+        logger.info("Names were added to collection 1");
+
         name1.add("Ivan");
         name1.add("Petr");
         name1.add("Olena");
         name1.add("Nickolay");
+
+        logger.info("Names were added to collection 2 ");
 
         assertEquals(name, name1);
 
         years.add(26);
         years.add(42);
 
+        logger.debug("Years were added to collection 1 ");
+
         years1.add(26);
         years1.add(42);
+
+        logger.debug("Years were added to collection 2 ");
 
         assertEquals(years, years1);
 
@@ -48,8 +57,9 @@ public class LinkedListTest {
         years.get(0);
         years1.get(0);
 
-        assertNotEquals(years, years1);
+        logger.info("Get years");
 
+        assertNotEquals(years, years1);
     }
 
     @Test
@@ -65,6 +75,9 @@ public class LinkedListTest {
         name1.remove(1);
 
         assertEquals(name, name1);
+
+        logger.info("Remove indexes");
+
     }
 
 }
