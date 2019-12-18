210511b87003aaa2de791a881acb3167e12b5266
erinus1 <sofiiabondarenko91@gmail.com>
2019-12-18 17:44:24 +0200

    +logger

--- src/Selenium_9/faceBook/FirstTest.java
+++ src/Selenium_9/faceBook/FirstTest.java
@@ -1,11 +1,15 @@
 package Selenium_9.faceBook;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class FirstTest {
+
+    final static Logger logger = LogManager.getLogger(FirstTest.class);
 
     @Test
     public void firstTest() throws InterruptedException {
@@ -29,11 +33,10 @@ public class FirstTest {
         try {
             driver.findElement(By.cssSelector("div.item-login > span.name.ellipsis")).click();
             textFound = true;
-            System.out.println("You are login");
+            logger.info("You are login");
 
-        } catch (Exception e) {
-            textFound = false;
-
+        } catch (Exception elementNotFound) {
+           logger.info("Element was not found");
         }
         driver.quit();
     }
