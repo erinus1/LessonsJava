ffe99b979d4a208580272cc79a247b853fcd7a76
erinus1 <sofiiabondarenko91@gmail.com>
2020-01-05 20:26:25 +0200

    check out

---
+++ src/main/Selenium_9Module/Task7/P141_Resizible.java
@@ -0,0 +1,75 @@
+package main.Selenium_9Module.Task7;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Point;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.concurrent.TimeUnit;
+
+/*
+Open https://demoqa.com/resizable/
+Get the current size of the rectangle  and print it to console
+Resize the rectangle  (make it  bigger)
+Get the current size of the rectangle  and print it to console
+Resize the rectangle  (make it  smaller)
+Get the current size of the rectangle  and print it to console
+
+ */
+
+public class P141_Resizible {
+
+    final Logger logger = LogManager.getLogger(P141_Resizible.class);
+    ChromeDriver driver = driverInit();
+
+    public ChromeDriver driverInit() {
+        String exePath = "src\\drivers\\chrome\\chromedriver.exe";
+        System.setProperty("webdriver.chrome.driver", exePath);
+        return new ChromeDriver();
+    }
+
+    @Before
+    public void set() {
+        driver.get("https://demoqa.com/resizable/");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+    }
+
+    public void getCoord(WebElement cub) {
+        WebElement cube = driver.findElement(By.xpath("//*[@id=\"resizable\"]/div[3]"));
+        Point point = cube.getLocation();
+        int xcord = point.getX();
+        logger.info("Position of the element of horizontal position: " + xcord + " pixels");
+        int ycord = point.getY();
+        logger.info("Position of the element of vertical position: " + ycord + " pixels");
+
+    }
+
+    @Test
+
+    public void resize() {
+
+        WebElement cube = driver.findElement(By.xpath("//*[@id=\"resizable\"]/div[3]"));
+        Actions builder = new Actions(driver);
+        builder.dragAndDropBy(cube, 400, 200).build().perform();
+        logger.info("Bigger cube: ");
+        getCoord(cube);
+
+        driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+
+        builder.dragAndDropBy(cube, -50, -50).build().perform();
+        logger.info("Smaller cube: ");
+        getCoord(cube);
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+
+        driver.quit();
+    }
+}
