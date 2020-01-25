9d6fc982d6d5964622978f681827d003db57a5f1
erinus1 <sofiiabondarenko91@gmail.com>
2020-01-25 16:07:33 +0200

    dosn't work as need (line 39-42)

--- src/main/Selenium_9Module/Task5/P139_Sortable.java
+++ src/main/Selenium_9Module/Task5/P139_Sortable.java
@@ -1,4 +1,5 @@
 package main.Selenium_9Module.Task5;
+
 import main.Selenium_9Module.Driver;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -8,30 +9,42 @@ import org.openqa.selenium.interactions.Actions;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
-//page 139
+/*
+Open https://demoqa.com/sortable/
+Check that all the items now are  sorted correctly
+Move the 2nd element after the 5th one
+ */
 public class P139_Sortable extends Driver {
     public static final Logger logger = LogManager.getLogger();
 
     @Test
-    public void sortItem() {
+    public void sortItem() throws InterruptedException {
         driverInit();
         driver.get("https://demoqa.com/sortable/");
         driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
         List<WebElement> menuItems = driver.findElements(By.cssSelector("[id='sortable']"));
 
         for(WebElement item: menuItems) {
-            logger.info("Custom sorting: " + item.getText());
+            logger.info("Custom sorting: " + '\n' +item.getText());
         }
-            Actions builder = new Actions(driver);
-            WebElement source = driver.findElement(By.xpath("//li[contains(.,'Item 2')]"));
-            WebElement target = driver.findElement(By.xpath("//li[contains(.,'Item 5')]"));
-            builder.dragAndDrop(source,target).perform();
-        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        WebElement source = driver.findElement(By.xpath("//li[contains(.,'Item 2')]"));
+        WebElement target = driver.findElement(By.xpath("//li[contains(.,'Item 5')]"));
+
+        Actions builder = new Actions(driver);
+
+        int x=target.getLocation().getX();
+        int y=target.getLocation().getY();
+
+        //builder.moveToElement(source).clickAndHold();
+        //builder.moveToElement(target).clickAndHold(target).perform();
+
+        builder.dragAndDropBy(source, x, y).clickAndHold(source).perform();
 
         for(WebElement item: menuItems) {
-            logger.info("New sorting: " + item.getText());
+            logger.info("New sorting: " +'\n'+ item.getText());
             }
+        //driver.quit();
         }
     }
 
